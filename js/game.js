3e39acd7c3759a6aaf95eaafde9bb5b0ee54ad55
Caleb Warner <cwarner@vertafore.com>
2025-04-03 18:14:36 -0400

    Feature additions and balancing

--- js/game.js
+++ js/game.js
@@ -95,20 +95,9 @@ BROWNIE.addEventListener("animationend", () => BROWNIE.classList.remove("mobile-
             item.bpps = bpps;
             pointsPerSecond += bpps;
         });
+
+        pointsPerSecond += storeUpgrades["excelerator"].total;
     }
-
-
-    // function addPointsPerSecond(addPointsPerSecond) {
-    //     pointsPerSecond += addPointsPerSecond;
-    //     renderObjects();
-    //     return pointsPerSecond;
-    // }
-
-    // function removePointsPerSecond(removePointsPerSecond) {
-    //     pointsPerSecond -= removePointsPerSecond;
-    //     renderObjects();
-    //     return pointsPerSecond;
-    // }
 
 
 
@@ -135,35 +124,71 @@ BROWNIE.addEventListener("animationend", () => BROWNIE.classList.remove("mobile-
             "amount": 0,
             "cost": 100,
             "costFactor": 1.2,
-            "baseMultiplier": 0.1,
+            "baseMultiplier": 0.2,
             "upgradeMultiplier": 1,
             "bpps": 0.0
-        }//,
-        // "oven": {
-        //     "amount": 0,
-        //     "cost": 1000,
-        //     "costFactor": 1.1,
-        //     "baseMultiplier": 1.0,
-        //     "upgradeMultiplier": 1,
-        //     "bpps": 0.0
-        // }
+        },
+        "oven-tronic": {
+            "amount": 0,
+            "cost": 5000,
+            "costFactor": 1.4,
+            "baseMultiplier": 10.0,
+            "upgradeMultiplier": 1,
+            "bpps": 0.0
+        },
+        "brownie-elf": {
+            "amount": 0,
+            "cost": 100000,
+            "costFactor": 1.4,
+            "baseMultiplier": 10000.0,
+            "upgradeMultiplier": 1,
+            "bpps": 0.0
+        }
     };
 
     let storeUpgrades = {
         "cursor-boost": {
             "amount": 0,
             "cost": 10,
-            "costFactor": 1.2,
+            "costFactor": 1.21,
             "total": 1,
-            "rate": 1
-        }//,
-        // "auto-clicker-boost": {
-        //     "amount": 0,
-        //     "cost": 200,
-        //     "costFactor": 1.3,
-        //     "total": 1,
-        //     "rate": 1.0
-        // }
+            "rate": 1.2
+        },
+        "mega-clicker": {
+            "amount": 0,
+            "cost": 1000,
+            "costFactor": 2.001,
+            "total": 1,
+            "rate": 2
+        },
+        "double-door": {
+            "amount": 0,
+            "cost": 10000,
+            "costFactor": 1.35,
+            "total": 1,
+            "rate": 1.5
+        },
+        "caffeine-kick": {
+            "amount": 0,
+            "cost": 1000000,
+            "costFactor": 2.5,
+            "total": 1,
+            "rate": 1.2
+        },
+        "more-chocolate": {
+            "amount": 0,
+            "cost": 1000000,
+            "costFactor": 3.0,
+            "total": 1,
+            "rate": 2.0
+        },
+        "excelerator": {
+            "amount": 0,
+            "cost": 100000000,
+            "costFactor": 3.0,
+            "total": 0.0,
+            "rate": 0.01
+        }
     };
 
 
@@ -218,6 +243,11 @@ BROWNIE.addEventListener("animationend", () => BROWNIE.classList.remove("mobile-
             let storeSelector = ".store-section .item-container." + key + " span." + key;
             document.querySelector(storeSelector + "-cost").innerHTML = pointsDisplay(item.cost);
             document.querySelector(storeSelector + "-each").innerHTML = perSecondDisplay(item.baseMultiplier * item.upgradeMultiplier);
+            if (browniePoints < item.cost) {
+                document.querySelector(storeSelector + "-cost").parentNode.parentNode.classList.add("empty");
+            } else {
+                document.querySelector(storeSelector + "-cost").parentNode.parentNode.classList.remove("empty");
+            }
         });
 
         let upgradeKeys = Object.keys(storeUpgrades);
@@ -234,6 +264,11 @@ BROWNIE.addEventListener("animationend", () => BROWNIE.classList.remove("mobile-
             let storeSelector = ".store-section .upgrade-container." + key + " span." + key;
             document.querySelector(storeSelector + "-cost").innerHTML = pointsDisplay(upgrade.cost);
             document.querySelector(storeSelector + "-rate").innerHTML = perSecondDisplay(upgrade.rate);
+            if (browniePoints < upgrade.cost) {
+                document.querySelector(storeSelector + "-cost").parentNode.parentNode.classList.add("empty");
+            } else {
+                document.querySelector(storeSelector + "-cost").parentNode.parentNode.classList.remove("empty");
+            }
         });
 
         POINTS_DISPLAY.innerHTML = pointsDisplay(browniePoints);
@@ -245,22 +280,22 @@ BROWNIE.addEventListener("animationend", () => BROWNIE.classList.remove("mobile-
     function pointsDisplay(points) {
         if (points > 999999999999999) {
             return (999.999).toFixed(3) + " T";
-        } else if (points > 1000000000000) {
+        } else if (points >= 1000000000000) {
             points /= 1000000000;
             points = Math.floor(points);
             points /= 1000;
             return points.toFixed(3) + " T";
-        } else if (points > 1000000000) {
+        } else if (points >= 1000000000) {
             points /= 1000000;
             points = Math.floor(points);
             points /= 1000;
             return points.toFixed(3) + " B";
-        } else if (points > 1000000) {
+        } else if (points >= 1000000) {
             points /= 1000;
             points = Math.floor(points);
             points /= 1000;
             return points.toFixed(3) + " M";
-        } else if (points > 1000) {
+        } else if (points >= 1000) {
             points = Math.floor(points);
             points /= 1000;
             return points.toFixed(3) + " K";
@@ -272,22 +307,22 @@ BROWNIE.addEventListener("animationend", () => BROWNIE.classList.remove("mobile-
     function perSecondDisplay(perSecond) {
         if (perSecond > 999999999999999) {
             return (999.999).toFixed(3) + " T";
-        } else if (perSecond > 1000000000000) {
+        } else if (perSecond >= 1000000000000) {
             perSecond /= 100000000000;
             perSecond = Math.floor(perSecond);
             perSecond /= 10;
             return perSecond.toFixed(1) + " T";
-        } else if (perSecond > 1000000000) {
+        } else if (perSecond >= 1000000000) {
             perSecond /= 100000000;
             perSecond = Math.floor(perSecond);
             perSecond /= 10;
             return perSecond.toFixed(1) + " B";
-        } else if (perSecond > 1000000) {
+        } else if (perSecond >= 1000000) {
             perSecond /= 100000;
             perSecond = Math.floor(perSecond);
             perSecond /= 10;
             return perSecond.toFixed(1) + " M";
-        } else if (perSecond > 1000) {
+        } else if (perSecond >= 1000) {
             perSecond /= 100;
             perSecond = Math.floor(perSecond);
             perSecond /= 10;
@@ -306,27 +341,103 @@ BROWNIE.addEventListener("animationend", () => BROWNIE.classList.remove("mobile-
 
     /* Specific store item implementations */
 
+
+    /* Items */
+    const AUTO_CLICKER = document.querySelector(".store-section .item-container.auto-clicker");
+    function autoClickerPurchase() {
+
+    }
+    AUTO_CLICKER.addEventListener("click", () => clickStoreItem("auto-clicker", autoClickerPurchase));
+
+
+
+    const OVEN_TRONIC = document.querySelector(".store-section .item-container.oven-tronic");
+    function ovenTronicPurchase() {
+
+    }
+    OVEN_TRONIC.addEventListener("click", () => clickStoreItem("oven-tronic", ovenTronicPurchase));
+
+
+
+    const BROWNIE_ELF = document.querySelector(".store-section .item-container.brownie-elf");
+    function brownieElfPurchase() {
+
+    }
+    BROWNIE_ELF.addEventListener("click", () => clickStoreItem("brownie-elf", brownieElfPurchase));
+
+
+
+
+
+
+
+
+
+
+    /* Upgrades */
+
     const CURSOR_BOOST = document.querySelector(".store-section .upgrade-container.cursor-boost");
     function cursorBoostPurchase() {
         let upgrade = storeUpgrades["cursor-boost"];
-        upgrade.total += upgrade.rate;
-        upgrade.rate++;
+        upgrade.total *= upgrade.rate;
     }
     CURSOR_BOOST.addEventListener("click", () => clickStoreItem("cursor-boost", cursorBoostPurchase));
 
 
 
-    const AUTO_CLICKER = document.querySelector(".store-section .item-container.auto-clicker");
-    function autoClickerPurchase() {
-        // addPointsPerSecond(0.1);
-        // let item = storeItems["auto-clicker"];
-        // item.
-
-        // calculatePoints
+    const MEGA_CLICKER = document.querySelector(".store-section .upgrade-container.mega-clicker");
+    function megaClickerPurchase() {
+        let upgrade = storeUpgrades["mega-clicker"];
+        upgrade.total *= upgrade.rate;
+        storeItems["auto-clicker"].upgradeMultiplier *= upgrade.rate;
     }
-    AUTO_CLICKER.addEventListener("click", () => clickStoreItem("auto-clicker", autoClickerPurchase));
+    MEGA_CLICKER.addEventListener("click", () => clickStoreItem("mega-clicker", megaClickerPurchase));
 
 
+
+    const DOUBLE_DOOR = document.querySelector(".store-section .upgrade-container.double-door");
+    function doubleDoorPurchase() {
+        let upgrade = storeUpgrades["double-door"];
+        upgrade.total *= upgrade.rate;
+        storeItems["oven-tronic"].upgradeMultiplier *= upgrade.rate;
+    }
+    DOUBLE_DOOR.addEventListener("click", () => clickStoreItem("double-door", doubleDoorPurchase));
+
+
+
+    const CAFFEINE_KICK = document.querySelector(".store-section .upgrade-container.caffeine-kick");
+    function caffeineKickPurchase() {
+        let upgrade = storeUpgrades["caffeine-kick"];
+        upgrade.total *= upgrade.rate;
+        storeItems["brownie-elf"].upgradeMultiplier *= upgrade.rate;
+    }
+    CAFFEINE_KICK.addEventListener("click", () => clickStoreItem("caffeine-kick", caffeineKickPurchase));
+
+
+    const MORE_CHOCOLATE = document.querySelector(".store-section .upgrade-container.more-chocolate");
+    function moreChocolatePurchase() {
+        let upgrade = storeUpgrades["more-chocolate"];
+        upgrade.total *= upgrade.rate;
+
+        Object.keys(storeItems).forEach((item) => {
+            storeItems[item].upgradeMultiplier *= 2;
+        });
+
+    }
+    MORE_CHOCOLATE.addEventListener("click", () => clickStoreItem("more-chocolate", moreChocolatePurchase));
+
+
+
+    const EXCELERATOR = document.querySelector(".store-section .upgrade-container.excelerator");
+    function exceleratorInterval() {
+        let upgrade = storeUpgrades["excelerator"];
+        upgrade.total += (pointsPerSecond - storeUpgrades["excelerator"].total) * 0.005;
+        calculatePointsPerSecond();
+    }
+    function exceleratorPurchase() {
+        setInterval(exceleratorInterval, 1000);
+    }
+    EXCELERATOR.addEventListener("click", () => clickStoreItem("excelerator", exceleratorPurchase));
 
 
 
